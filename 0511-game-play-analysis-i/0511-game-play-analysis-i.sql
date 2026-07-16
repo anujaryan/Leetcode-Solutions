1bf7f7f09905cf1f3152cdb3f0846ad91a1781ec
Anuj Kumar <156888339+anujaryan@users.noreply.github.com>
2026-07-16 18:18:58 +0530

    Time: 583 ms (18.13%), Space: 0B (100%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select player_id , min(event_date) as first_login from activity
+group by player_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
